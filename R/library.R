4c2541b1592166ae7fc0638c9b91a0656bf2a14d
Anton <anton.camacho@lshtm.ac.uk>
2016-10-19 11:40:30 +0200

    common lib

---
+++ R/library.R
@@ -0,0 +1,37 @@
+# data manipulation
+library(tidyverse)
+library(hms)
+library(stringr)
+library(lubridate)
+library(forcats)
+library(ISOweek)
+
+# import
+library(readxl)
+library(rvest)
+library(curl)
+
+# viz
+library(ggmap)
+library(RColorBrewer)
+
+# markdown
+library(pander)
+
+# spatial
+library(rgeos)
+library(sp)
+library(maptools)
+library(rgdal)
+
+
+# htmlwidget
+library(DT)
+library(plotly)
+# library(rMaps)
+# library(rCharts)
+
+# local functions
+source("../R/utils.r")
+
+
