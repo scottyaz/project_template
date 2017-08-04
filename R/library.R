a073658be0b6f0932162c9c8752b78aa35bd2ea3
Anton <anton.camacho@lshtm.ac.uk>
2017-08-04 10:49:14 +0200

    update default libraries

--- R/library.R
+++ R/library.R
@@ -1,35 +1,43 @@
 # data manipulation
 library(tidyverse)
+library(tidyr)
 library(hms)
 library(stringr)
 library(lubridate)
 library(forcats)
 library(ISOweek)
+library(EpiWeek)
+library(stringi)
 
 # import
 library(readxl)
 library(rvest)
 library(curl)
 
-# viz
+# static viz
 library(ggmap)
 library(RColorBrewer)
+library(ggrepel)
 
 # markdown
 library(pander)
+library(rmarkdown)
+library(brew)
 
 # spatial
 library(rgeos)
 library(sp)
 library(maptools)
 library(rgdal)
-
+library(ggsn)
+library(rmapshaper)
 
 # htmlwidget
 library(DT)
 library(plotly)
-# library(rMaps)
-# library(rCharts)
+library(sparkline)
+library(leaflet)
+library(leaflet.extra)
 
 # local functions
 source("../R/utils.r")
