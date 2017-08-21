51024e5166f9368cdc5a021b02deb59d4c1a701e
Anton <anton.camacho@lshtm.ac.uk>
2017-08-21 16:21:02 +0200

    add google analytics

--- R/dev.r
+++ R/dev.r
@@ -31,12 +31,17 @@ render_html <- function(input_names = NULL, dir_Rmd, dir_website, dir_html, rm_c
 
 	input_static <- c("data_cleaning")
 
+	input_no_google_analytics <- c("data_cleaning")
+
+	has_google_analytics <- file.exits(file.path(dir_Rmd, "google_analytics.html"))
+
 	for(input_name in input_names){
 
 		input <- file.path(dir_Rmd, sprintf("%s.Rmd", input_name))
 		has_toc <- !input_name%in%input_no_toc
 		number_sections <- input_name %in% input_number_sections
 		is_static <- input_name %in% input_static
+		add_google_analytics <- (has_google_analytics & !(input_name %in% input_no_google_analytics))
 
 		if(is_static){
 			dir_output <- dir_html
@@ -53,21 +58,22 @@ render_html <- function(input_names = NULL, dir_Rmd, dir_website, dir_html, rm_c
 			}
 		}
 
-
 		if(rm_cache) {
 			unlink(sprintf("Rmd/%s_cache", input_name), recursive = TRUE)
 		}
 
-		render(input = input, output_dir = dir_output, output_format = html_document(
-			self_contained = is_static, 
-			lib_dir = ifelse(is_static, NULL, file.path(dir_output, "libs")), 
-			toc = has_toc, 
-			toc_float = has_toc, 
-			code_folding = code_folding, 
-			number_sections = number_sections, 
-			theme = "yeti", 
-			highlight = "tango"
-			))
+		render(input = input, output_dir = dir_output, output_format = 
+			html_document(
+				self_contained = is_static, 
+				lib_dir = ifelse(is_static, NULL, file.path(dir_output, "libs")), 
+				toc = has_toc, 
+				toc_float = has_toc, 
+				code_folding = code_folding, 
+				number_sections = number_sections, 
+				theme = "yeti", 
+				highlight = "tango",
+				includes = switch(add_google_analytics+1, NULL, includes(in_header = file.path(dir_Rmd, "google_analytics.html")))
+				))
 
 		if(is_static){
 			site_file <- "Rmd/site.yml"
@@ -79,7 +85,7 @@ render_html <- function(input_names = NULL, dir_Rmd, dir_website, dir_html, rm_c
 
 }
 
-setup_gitignore <- function(dir_project) {
+setup_gitignore <- function(dir_project, force = FALSE) {
 
 	# remove usual directories from version-control
 	x <- "
@@ -97,10 +103,34 @@ setup_gitignore <- function(dir_project) {
 
 	gitignore <- file.path(dir_project, ".gitignore")
 
-	if(!file.exists(gitignore)){
+	if(!file.exists(gitignore) | force){
 		write(x, gitignore)		
 	} else {
 		warning(".gitignore already exists")
+	}
+
+}
+
+setup_google_analytics <- function(dir_Rmd, tracking_id, force = FALSE) {
+
+	x <- sprintf("
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', '%s', 'auto');
+	ga('send', 'pageview');
+
+	</script>", tracking_id)
+
+	ga_file <- file.path(dir_Rmd, "google_analytics.html")
+
+	if(!file.exists(ga_file) | force){
+		write(x, ga_file)		
+	} else {
+		warning("google_analytics.html already exists")
 	}
 
 }
@@ -133,6 +163,12 @@ main <- function() {
 
 	# run only once, then warn (unless force = TRUE)
 	setup_gitignore(dir_project, force = FALSE)
+	
+	# get a google analytics tracking number and everything should setup automatically
+	tracking_id <- NULL
+	if(!is.null(tracking_id)){
+		setup_google_analytics(dir_Rmd = dir_Rmd, tracking_id = tracking_id, force = FALSE)		
+	}
 
 	# initialize packrat or switch in packrat mode if already done
 	init_packrat(dir_project = dir_project)
